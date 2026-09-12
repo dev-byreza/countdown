6429b8e0685bedfb86891e6170b981505a20478f
dev-byreza <devbyreza@gmail.com>
2026-09-12 11:55:59 +0800

    Bust stale browser cache for countdown display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chronos-suite-v2';
+const CACHE_NAME = 'chronos-suite-v3';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
